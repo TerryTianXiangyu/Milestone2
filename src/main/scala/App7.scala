114209580e6a422af39304126703dcb8eb284585
TerryTianXiangyu <48174577+TerryTianXiangyu@users.noreply.github.com>
2020-04-03 10:52:06 +0200

    Update App7.scala

--- src/main/scala/App7.scala
+++ src/main/scala/App7.scala
@@ -21,8 +21,33 @@ object App7 {
       val c = line.split(",")
       (c(0), c(1).toInt)
     })
+
+    /* original version
     val filtered = rdd1.join(rdd2).filter { case (k, (v1, v2)) => v1 < v2 }
     val result = filtered.groupBy(_._1).map { case (k, l) => k -> l.map(_._2._1).sum }
     result.collect.foreach(println)
+    */
+
+    // edit from here
+    //save rdd2 as map, and broadcast it
+    val pairs = rdd2.collectAsMap
+    val broadCastMap = sc.broadcast(pairs)
+  
+    // use broadcast rdd and map side join
+    val rddjoin = rdd1.mapPartitions({ iter =>
+      val m = broadCastMap.value
+      for {
+        (key, value) <- iter
+        if (m.contains(key))
+      } yield (key, (value.toInt, m.get(key).getOrElse(0).toInt))
+    })
+
+    //filter on condition for selection, no change
+    val filtered = rddjoin.filter { case (k, (v1, v2)) => v1 < v2 }
+
+    //use reduceByKey to add the values 
+    val result = filtered.map {case (k, (_, v2)) => (k, v2)}.reduceByKey(_ + _)
+    result.collect.foreach(println)
+
   }
 }
